01efa75f512bcdba6f48ce7ccf2400957cd7599b
mmo <thetechpomelo@gmail.com>
2022-05-18 18:46:35 +0200

    add no metamask case

--- components/connectMetamask.js
+++ components/connectMetamask.js
@@ -1,31 +1,49 @@
 import { useWeb3React } from '@web3-react/core'
 import { injected } from '../connectors';
+import React, { useState, useEffect } from "react";
 //
 import { Button } from '@mui/material';
 
+
 export default function ConnectMetamask() {
     const { active, account, chainId, activate, deactivate } = useWeb3React();
+    const [hasMetamask, setHasMetamask] = useState(false);
 
+    useEffect(() => {
+        if (typeof window.ethereum !== "undefined") {
+            setHasMetamask(true);
+        }
+    });
+
+    if (!hasMetamask) {
+        return (
+            <div>
+                <a href="https://metamask.io/" target="_blank" rel="noreferrer">Please install Metamask</a>
+            </div >
+        )
+    }
     if (!active) {
         return (
             <div >
                 <Button href="#" variant="outlined" sx={{ my: 1, mx: 1.5 }}
                     onClick={() => { activate(injected) }}>Connect Metamask</Button>
-            </div>)
+            </div >
+        )
+
+    } else if (chainId == 4) {
+        return (
+            <div>{account}
+                < Button href="#" variant="text" sx={{ my: 1, mx: 1.5 }} color="error"
+                    onClick={deactivate} > disconnect</Button >
+            </div >
+        )
 
     } else {
         return (
-
             <div >
-                {chainId == 4 ? (
-                    <div>
-                        {account}
-                        < Button href="#" variant="text" sx={{ my: 1, mx: 1.5 }} color="error"
-                            onClick={deactivate}>disconnect</Button>
-                    </div >) :
-                    (alert("please change to the rinkeby testnet"))
-                }
-            </div >)
+                {alert("please change to the rinkeby testnet")}
+            </div >
+        )
     }
 
 }
